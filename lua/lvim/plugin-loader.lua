4aab8b966296fc6ef3fd9601bb75e14d78a31c95
gh-drive <148326012+gh-drive@users.noreply.github.com>
2024-07-09 20:12:27 +0800

    chore: disable unused plugins chore: use mirror

--- lua/lvim/plugin-loader.lua
+++ lua/lvim/plugin-loader.lua
@@ -1,7 +1,7 @@
 local plugin_loader = {}
 
-local utils = require "lvim.utils"
-local Log = require "lvim.core.log"
+local utils = require("lvim.utils")
+local Log = require("lvim.core.log")
 local join_paths = utils.join_paths
 
 local plugins_dir = join_paths(get_runtime_dir(), "site", "pack", "lazy", "opt")
@@ -9,34 +9,33 @@ local plugins_dir = join_paths(get_runtime_dir(), "site", "pack", "lazy", "opt")
 function plugin_loader.init(opts)
   opts = opts or {}
 
-  local lazy_install_dir = opts.install_path
-    or join_paths(vim.fn.stdpath "data", "site", "pack", "lazy", "opt", "lazy.nvim")
+  local lazy_install_dir = opts.install_path or join_paths(vim.fn.stdpath("data"), "site", "pack", "lazy", "opt", "lazy.nvim")
 
   if not utils.is_directory(lazy_install_dir) then
-    print "Initializing first time setup"
+    print("Initializing first time setup")
     local core_plugins_dir = join_paths(get_lvim_base_dir(), "plugins")
     if utils.is_directory(core_plugins_dir) then
       vim.fn.mkdir(plugins_dir, "p")
       vim.fn.delete(plugins_dir, "rf")
       require("lvim.utils").fs_copy(core_plugins_dir, plugins_dir)
     else
-      vim.fn.system {
+      vim.fn.system({
         "git",
         "clone",
         "--branch=stable",
-        "https://github.com/folke/lazy.nvim.git",
+        "https://e.coding.net/zydou/vim/lazy.nvim.git",
         lazy_install_dir,
-      }
+      })
 
       local default_snapshot_path = join_paths(get_lvim_base_dir(), "snapshots", "default.json")
       local snapshot = assert(vim.fn.json_decode(vim.fn.readfile(default_snapshot_path)))
-      vim.fn.system {
+      vim.fn.system({
         "git",
         "-C",
         lazy_install_dir,
         "checkout",
         snapshot["lazy.nvim"].commit,
-      }
+      })
     end
 
     vim.api.nvim_create_autocmd("User", { pattern = "LazyDone", callback = require("lvim.lsp").setup })
@@ -58,14 +57,14 @@ function plugin_loader.init(opts)
 
   pcall(function()
     -- set a custom path for lazy's cache
-    local lazy_cache = require "lazy.core.cache"
+    local lazy_cache = require("lazy.core.cache")
     lazy_cache.path = join_paths(get_cache_dir(), "lazy", "luac")
   end)
 end
 
 function plugin_loader.reload(spec)
-  local Config = require "lazy.core.config"
-  local lazy = require "lazy"
+  local Config = require("lazy.core.config")
+  local lazy = require("lazy")
 
   -- TODO: reset cache? and unload plugins?
 
@@ -74,27 +73,23 @@ function plugin_loader.reload(spec)
   require("lazy.core.plugin").load(true)
   require("lazy.core.plugin").update_state()
 
-  local not_installed_plugins = vim.tbl_filter(function(plugin)
-    return not plugin._.installed
-  end, Config.plugins)
+  local not_installed_plugins = vim.tbl_filter(function(plugin) return not plugin._.installed end, Config.plugins)
 
   require("lazy.manage").clear()
 
-  if #not_installed_plugins > 0 then
-    lazy.install { wait = true }
-  end
+  if #not_installed_plugins > 0 then lazy.install({ wait = true }) end
 
   if #Config.to_clean > 0 then
     -- TODO: set show to true when lazy shows something useful on clean
-    lazy.clean { wait = true, show = false }
+    lazy.clean({ wait = true, show = false })
   end
 end
 
 function plugin_loader.load(configurations)
-  Log:debug "loading plugins configuration"
+  Log:debug("loading plugins configuration")
   local lazy_available, lazy = pcall(require, "lazy")
   if not lazy_available then
-    Log:warn "skipping loading plugins until lazy.nvim is installed"
+    Log:warn("skipping loading plugins until lazy.nvim is installed")
     return
   end
 
@@ -107,19 +102,17 @@ function plugin_loader.load(configurations)
   end, debug.traceback)
 
   if not status_ok then
-    Log:warn "problems detected while loading plugins' configurations"
+    Log:warn("problems detected while loading plugins' configurations")
     Log:trace(debug.traceback())
   end
 end
 
 function plugin_loader.get_core_plugins()
   local names = {}
-  local plugins = require "lvim.plugins"
+  local plugins = require("lvim.plugins")
   local get_name = require("lazy.core.plugin").Spec.get_name
   for _, spec in pairs(plugins) do
-    if spec.enabled == true or spec.enabled == nil then
-      table.insert(names, get_name(spec[1]))
-    end
+    if spec.enabled == true or spec.enabled == nil then table.insert(names, get_name(spec[1])) end
   end
   return names
 end
@@ -127,12 +120,12 @@ end
 function plugin_loader.sync_core_plugins()
   local core_plugins = plugin_loader.get_core_plugins()
   Log:trace(string.format("Syncing core plugins: [%q]", table.concat(core_plugins, ", ")))
-  require("lazy").update { wait = true, plugins = core_plugins }
+  require("lazy").update({ wait = true, plugins = core_plugins })
 end
 
 function plugin_loader.ensure_plugins()
-  Log:debug "calling lazy.install()"
-  require("lazy").install { wait = true }
+  Log:debug("calling lazy.install()")
+  require("lazy").install({ wait = true })
 end
 
 return plugin_loader
--- lua/lvim/plugins.lua
+++ lua/lvim/plugins.lua
@@ -1,71 +1,63 @@
 -- local require = require("lvim.utils.require").require
 local core_plugins = {
-  { "folke/lazy.nvim", tag = "stable" },
+  { url = "https://e.coding.net/zydou/vim/lazy.nvim.git", tag = "stable" },
   {
-    "neovim/nvim-lspconfig",
+    url = "https://e.coding.net/zydou/vim/nvim-lspconfig.git",
     lazy = true,
     dependencies = { "mason-lspconfig.nvim", "nlsp-settings.nvim" },
   },
   {
-    "williamboman/mason-lspconfig.nvim",
+    url = "https://e.coding.net/zydou/vim/mason-lspconfig.nvim.git",
     cmd = { "LspInstall", "LspUninstall" },
     config = function()
       require("mason-lspconfig").setup(lvim.lsp.installer.setup)
 
       -- automatic_installation is handled by lsp-manager
-      local settings = require "mason-lspconfig.settings"
+      local settings = require("mason-lspconfig.settings")
       settings.current.automatic_installation = false
     end,
     lazy = true,
     event = "User FileOpened",
     dependencies = "mason.nvim",
   },
-  { "tamago324/nlsp-settings.nvim", cmd = "LspSettings", lazy = true },
-  { "nvimtools/none-ls.nvim", lazy = true },
+  { url = "https://e.coding.net/zydou/vim/nlsp-settings.nvim.git", cmd = "LspSettings", lazy = true },
+  { url = "https://e.coding.net/zydou/vim/none-ls.nvim.git", lazy = true },
   {
-    "williamboman/mason.nvim",
-    config = function()
-      require("lvim.core.mason").setup()
-    end,
+    url = "https://e.coding.net/zydou/vim/mason.nvim.git",
+    config = function() require("lvim.core.mason").setup() end,
     cmd = { "Mason", "MasonInstall", "MasonUninstall", "MasonUninstallAll", "MasonLog" },
     build = function()
-      pcall(function()
-        require("mason-registry").refresh()
-      end)
+      pcall(function() require("mason-registry").refresh() end)
     end,
     event = "User FileOpened",
     lazy = true,
   },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = not vim.startswith(lvim.colorscheme, "tokyonight"),
+  -- },
   {
-    "folke/tokyonight.nvim",
-    lazy = not vim.startswith(lvim.colorscheme, "tokyonight"),
-  },
-  {
-    "lunarvim/lunar.nvim",
+    url = "https://e.coding.net/zydou/vim/lunar.nvim.git",
     lazy = lvim.colorscheme ~= "lunar",
   },
-  { "Tastyep/structlog.nvim", lazy = true },
-  { "nvim-lua/plenary.nvim", cmd = { "PlenaryBustedFile", "PlenaryBustedDirectory" }, lazy = true },
+  { url = "https://e.coding.net/zydou/vim/structlog.nvim.git", lazy = true },
+  { url = "https://e.coding.net/zydou/vim/plenary.nvim.git", cmd = { "PlenaryBustedFile", "PlenaryBustedDirectory" }, lazy = true },
   -- Telescope
   {
-    "nvim-telescope/telescope.nvim",
+    url = "https://e.coding.net/zydou/vim/telescope.nvim.git",
     branch = "0.1.x",
-    config = function()
-      require("lvim.core.telescope").setup()
-    end,
+    config = function() require("lvim.core.telescope").setup() end,
     dependencies = { "telescope-fzf-native.nvim" },
     lazy = true,
     cmd = "Telescope",
     enabled = lvim.builtin.telescope.active,
   },
-  { "nvim-telescope/telescope-fzf-native.nvim", build = "make", lazy = true, enabled = lvim.builtin.telescope.active },
+  { url = "https://e.coding.net/zydou/vim/telescope-fzf-native.nvim.git", build = "make", lazy = true, enabled = lvim.builtin.telescope.active },
   -- Install nvim-cmp, and buffer source as a dependency
   {
-    "hrsh7th/nvim-cmp",
+    url = "https://e.coding.net/zydou/vim/nvim-cmp.git",
     config = function()
-      if lvim.builtin.cmp then
-        require("lvim.core.cmp").setup()
-      end
+      if lvim.builtin.cmp then require("lvim.core.cmp").setup() end
     end,
     event = { "InsertEnter", "CmdlineEnter" },
     dependencies = {
@@ -76,31 +68,27 @@ local core_plugins = {
       "cmp-cmdline",
     },
   },
-  { "hrsh7th/cmp-nvim-lsp", lazy = true },
-  { "saadparwaiz1/cmp_luasnip", lazy = true },
-  { "hrsh7th/cmp-buffer", lazy = true },
-  { "hrsh7th/cmp-path", lazy = true },
+  { url = "https://e.coding.net/zydou/vim/cmp-nvim-lsp.git", lazy = true },
+  { url = "https://e.coding.net/zydou/vim/cmp_luasnip.git", lazy = true },
+  { url = "https://e.coding.net/zydou/vim/cmp-buffer.git", lazy = true },
+  { url = "https://e.coding.net/zydou/vim/cmp-path.git", lazy = true },
   {
-    "hrsh7th/cmp-cmdline",
+    url = "https://e.coding.net/zydou/vim/cmp-cmdline.git",
     lazy = true,
     enabled = lvim.builtin.cmp and lvim.builtin.cmp.cmdline.enable or false,
   },
   {
-    "L3MON4D3/LuaSnip",
+    url = "https://github.com/L3MON4D3/LuaSnip.git", -- has submodule, do not use mirror url
     config = function()
-      local utils = require "lvim.utils"
+      local utils = require("lvim.utils")
       local paths = {}
-      if lvim.builtin.luasnip.sources.friendly_snippets then
-        paths[#paths + 1] = utils.join_paths(get_runtime_dir(), "site", "pack", "lazy", "opt", "friendly-snippets")
-      end
+      if lvim.builtin.luasnip.sources.friendly_snippets then paths[#paths + 1] = utils.join_paths(get_runtime_dir(), "site", "pack", "lazy", "opt", "friendly-snippets") end
       local user_snippets = utils.join_paths(get_config_dir(), "snippets")
-      if utils.is_directory(user_snippets) then
-        paths[#paths + 1] = user_snippets
-      end
+      if utils.is_directory(user_snippets) then paths[#paths + 1] = user_snippets end
       require("luasnip.loaders.from_lua").lazy_load()
-      require("luasnip.loaders.from_vscode").lazy_load {
+      require("luasnip.loaders.from_vscode").lazy_load({
         paths = paths,
-      }
+      })
       require("luasnip.loaders.from_snipmate").lazy_load()
     end,
     event = "InsertEnter",
@@ -108,29 +96,27 @@ local core_plugins = {
       "friendly-snippets",
     },
   },
-  { "rafamadriz/friendly-snippets", lazy = true, cond = lvim.builtin.luasnip.sources.friendly_snippets },
+  { url = "https://e.coding.net/zydou/vim/friendly-snippets.git", lazy = true, cond = lvim.builtin.luasnip.sources.friendly_snippets },
   {
-    "folke/neodev.nvim",
+    url = "https://e.coding.net/zydou/vim/neodev.nvim.git",
     lazy = true,
   },
 
   -- Autopairs
   {
-    "windwp/nvim-autopairs",
+    url = "https://e.coding.net/zydou/vim/nvim-autopairs.git",
     event = "InsertEnter",
-    config = function()
-      require("lvim.core.autopairs").setup()
-    end,
+    config = function() require("lvim.core.autopairs").setup() end,
     enabled = lvim.builtin.autopairs.active,
     dependencies = { "nvim-treesitter/nvim-treesitter", "hrsh7th/nvim-cmp" },
   },
 
   -- Treesitter
   {
-    "nvim-treesitter/nvim-treesitter",
+    url = "https://e.coding.net/zydou/vim/nvim-treesitter.git",
     -- run = ":TSUpdate",
     config = function()
-      local utils = require "lvim.utils"
+      local utils = require("lvim.utils")
       local path = utils.join_paths(get_runtime_dir(), "site", "pack", "lazy", "opt", "nvim-treesitter")
       vim.opt.rtp:prepend(path) -- treesitter needs to be before nvim's runtime in rtp
       require("lvim.core.treesitter").setup()
@@ -148,34 +134,32 @@ local core_plugins = {
   },
   {
     -- Lazy loaded by Comment.nvim pre_hook
-    "JoosepAlviste/nvim-ts-context-commentstring",
+    url = "https://e.coding.net/zydou/vim/nvim-ts-context-commentstring.git",
     lazy = true,
   },
 
   -- NvimTree
-  {
-    "nvim-tree/nvim-tree.lua",
-    config = function()
-      require("lvim.core.nvimtree").setup()
-    end,
-    enabled = lvim.builtin.nvimtree.active,
-    cmd = { "NvimTreeToggle", "NvimTreeOpen", "NvimTreeFocus", "NvimTreeFindFileToggle" },
-    event = "User DirOpened",
-  },
+  -- {
+  --   "nvim-tree/nvim-tree.lua",
+  --   config = function()
+  --     require("lvim.core.nvimtree").setup()
+  --   end,
+  --   enabled = lvim.builtin.nvimtree.active,
+  --   cmd = { "NvimTreeToggle", "NvimTreeOpen", "NvimTreeFocus", "NvimTreeFindFileToggle" },
+  --   event = "User DirOpened",
+  -- },
   -- Lir
+  -- {
+  --   "tamago324/lir.nvim",
+  --   config = function()
+  --     require("lvim.core.lir").setup()
+  --   end,
+  --   enabled = lvim.builtin.lir.active,
+  --   event = "User DirOpened",
+  -- },
   {
-    "tamago324/lir.nvim",
-    config = function()
-      require("lvim.core.lir").setup()
-    end,
-    enabled = lvim.builtin.lir.active,
-    event = "User DirOpened",
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("lvim.core.gitsigns").setup()
-    end,
+    url = "https://e.coding.net/zydou/vim/gitsigns.nvim.git",
+    config = function() require("lvim.core.gitsigns").setup() end,
     event = "User FileOpened",
     cmd = "Gitsigns",
     enabled = lvim.builtin.gitsigns.active,
@@ -183,10 +167,8 @@ local core_plugins = {
 
   -- Whichkey
   {
-    "folke/which-key.nvim",
-    config = function()
-      require("lvim.core.which-key").setup()
-    end,
+    url = "https://e.coding.net/zydou/vim/which-key.nvim.git",
+    config = function() require("lvim.core.which-key").setup() end,
     cmd = "WhichKey",
     event = "VeryLazy",
     enabled = lvim.builtin.which_key.active,
@@ -194,29 +176,27 @@ local core_plugins = {
 
   -- Comments
   {
-    "numToStr/Comment.nvim",
-    config = function()
-      require("lvim.core.comment").setup()
-    end,
+    url = "https://e.coding.net/zydou/vim/Comment.nvim.git",
+    config = function() require("lvim.core.comment").setup() end,
     keys = { { "gc", mode = { "n", "v" } }, { "gb", mode = { "n", "v" } } },
     event = "User FileOpened",
     enabled = lvim.builtin.comment.active,
   },
 
   -- project.nvim
-  {
-    "ahmedkhalf/project.nvim",
-    config = function()
-      require("lvim.core.project").setup()
-    end,
-    enabled = lvim.builtin.project.active,
-    event = "VimEnter",
-    cmd = "Telescope projects",
-  },
+  -- {
+  --   "ahmedkhalf/project.nvim",
+  --   config = function()
+  --     require("lvim.core.project").setup()
+  --   end,
+  --   enabled = lvim.builtin.project.active,
+  --   event = "VimEnter",
+  --   cmd = "Telescope projects",
+  -- },
 
   -- Icons
   {
-    "nvim-tree/nvim-web-devicons",
+    url = "https://e.coding.net/zydou/vim/nvim-web-devicons.git",
     enabled = lvim.use_icons,
     lazy = true,
   },
@@ -224,78 +204,66 @@ local core_plugins = {
   -- Status Line and Bufferline
   {
     -- "hoob3rt/lualine.nvim",
-    "nvim-lualine/lualine.nvim",
+    url = "https://e.coding.net/zydou/vim/lualine.nvim.git",
     -- "Lunarvim/lualine.nvim",
-    config = function()
-      require("lvim.core.lualine").setup()
-    end,
+    config = function() require("lvim.core.lualine").setup() end,
     event = "VimEnter",
     enabled = lvim.builtin.lualine.active,
   },
 
   -- breadcrumbs
   {
-    "SmiteshP/nvim-navic",
-    config = function()
-      require("lvim.core.breadcrumbs").setup()
-    end,
+    url = "https://e.coding.net/zydou/vim/nvim-navic.git",
+    config = function() require("lvim.core.breadcrumbs").setup() end,
     event = "User FileOpened",
     enabled = lvim.builtin.breadcrumbs.active,
   },
 
   {
-    "akinsho/bufferline.nvim",
-    config = function()
-      require("lvim.core.bufferline").setup()
-    end,
+    url = "https://e.coding.net/zydou/vim/bufferline.nvim.git",
+    config = function() require("lvim.core.bufferline").setup() end,
     branch = "main",
     event = "User FileOpened",
     enabled = lvim.builtin.bufferline.active,
   },
 
   -- Debugging
-  {
-    "mfussenegger/nvim-dap",
-    config = function()
-      require("lvim.core.dap").setup()
-    end,
-    lazy = true,
-    dependencies = {
-      "rcarriga/nvim-dap-ui",
-    },
-    enabled = lvim.builtin.dap.active,
-  },
+  -- {
+  --   "mfussenegger/nvim-dap",
+  --   config = function()
+  --     require("lvim.core.dap").setup()
+  --   end,
+  --   lazy = true,
+  --   dependencies = {
+  --     "rcarriga/nvim-dap-ui",
+  --   },
+  --   enabled = lvim.builtin.dap.active,
+  -- },
 
   -- Debugger user interface
-  {
-    "rcarriga/nvim-dap-ui",
-    config = function()
-      require("lvim.core.dap").setup_ui()
-    end,
-    lazy = true,
-    enabled = lvim.builtin.dap.active,
-  },
+  -- {
+  --   "rcarriga/nvim-dap-ui",
+  --   config = function()
+  --     require("lvim.core.dap").setup_ui()
+  --   end,
+  --   lazy = true,
+  --   enabled = lvim.builtin.dap.active,
+  -- },
 
   -- alpha
   {
-    "goolord/alpha-nvim",
-    config = function()
-      require("lvim.core.alpha").setup()
-    end,
+    url = "https://e.coding.net/zydou/vim/alpha-nvim.git",
+    config = function() require("lvim.core.alpha").setup() end,
     enabled = lvim.builtin.alpha.active,
     event = "VimEnter",
   },
 
   -- Terminal
   {
-    "akinsho/toggleterm.nvim",
+    url = "https://e.coding.net/zydou/vim/toggleterm.nvim.git",
     branch = "main",
-    init = function()
-      require("lvim.core.terminal").init()
-    end,
-    config = function()
-      require("lvim.core.terminal").setup()
-    end,
+    init = function() require("lvim.core.terminal").init() end,
+    config = function() require("lvim.core.terminal").setup() end,
     cmd = {
       "ToggleTerm",
       "TermExec",
@@ -310,48 +278,42 @@ local core_plugins = {
 
   -- SchemaStore
   {
-    "b0o/schemastore.nvim",
+    url = "https://e.coding.net/zydou/vim/schemastore.nvim.git",
     lazy = true,
   },
 
   {
-    "RRethy/vim-illuminate",
-    config = function()
-      require("lvim.core.illuminate").setup()
-    end,
+    url = "https://e.coding.net/zydou/vim/vim-illuminate.git",
+    config = function() require("lvim.core.illuminate").setup() end,
     event = "User FileOpened",
     enabled = lvim.builtin.illuminate.active,
   },
 
   {
-    "lukas-reineke/indent-blankline.nvim",
-    config = function()
-      require("lvim.core.indentlines").setup()
-    end,
+    url = "https://e.coding.net/zydou/vim/indent-blankline.nvim.git",
+    config = function() require("lvim.core.indentlines").setup() end,
     event = "User FileOpened",
     enabled = lvim.builtin.indentlines.active,
   },
 
-  {
-    "lunarvim/onedarker.nvim",
-    branch = "freeze",
-    config = function()
-      pcall(function()
-        if lvim and lvim.colorscheme == "onedarker" then
-          require("onedarker").setup()
-          lvim.builtin.lualine.options.theme = "onedarker"
-        end
-      end)
-    end,
-    lazy = lvim.colorscheme ~= "onedarker",
-  },
+  -- {
+  --   "lunarvim/onedarker.nvim",
+  --   branch = "freeze",
+  --   config = function()
+  --     pcall(function()
+  --       if lvim and lvim.colorscheme == "onedarker" then
+  --         require("onedarker").setup()
+  --         lvim.builtin.lualine.options.theme = "onedarker"
+  --       end
+  --     end)
+  --   end,
+  --   lazy = lvim.colorscheme ~= "onedarker",
+  -- },
 
   {
-    "lunarvim/bigfile.nvim",
+    url = "https://e.coding.net/zydou/vim/bigfile.nvim.git",
     config = function()
-      pcall(function()
-        require("bigfile").setup(lvim.builtin.bigfile.config)
-      end)
+      pcall(function() require("bigfile").setup(lvim.builtin.bigfile.config) end)
     end,
     enabled = lvim.builtin.bigfile.active,
     dependencies = { "nvim-treesitter/nvim-treesitter" },
@@ -371,7 +333,7 @@ local get_short_name = function(long_name)
 end
 
 local get_default_sha1 = function(spec)
-  local short_name = get_short_name(spec[1])
+  local short_name = get_short_name(spec.url)
   return default_sha1[short_name] and default_sha1[short_name].commit
 end
 
